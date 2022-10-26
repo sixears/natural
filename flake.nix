19a63c237b649546e7df2ce5ecf6eb4104a53e33
Mr. Fluffy <git@sixearsm>
2022-10-26 08:11:50 +0000

    add flake

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Type-level natural numbers";
+
+  inputs = {
+    nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
+    build-utils.url  = "github:sixears/flake-build-utils/r0.0.0.0";
+    more-unicode.url = "github:sixears/more-unicode/r0.0.17.0";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, build-utils, more-unicode }:
+    build-utils.lib.hOutputs self nixpkgs "natural" { inherit more-unicode; };
+}
