ba8ac84bac1917f91fe1ccb5f31471529f19d4cd
Mr. Fluffy <git@sixearsm>
2022-11-19 06:38:55 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url  = github:sixears/flake-build-utils/r1.0.0.13;
 
-    more-unicode.url = github:sixears/more-unicode/r0.0.17.10;
+    more-unicode.url = github:sixears/more-unicode/r0.0.17.11;
   };
 
   outputs = { self, nixpkgs, build-utils
@@ -20,7 +20,7 @@
         in
           mkDerivation {
             pname = "natural";
-            version = "0.0.1.9";
+            version = "0.0.1.13";
             src = ./.;
             libraryHaskellDepends = [
               base base-unicode-symbols
